bf723f8d38ba0d8b84e081695da79ecc380ab028
Khalid <mahamud.0.khalid@gmail.com>
2023-08-19 02:47:03 +0600

    completed moblie menu

---
+++ build/js/script.js
@@ -0,0 +1,16 @@
+const hamburgerMenu = document.querySelector('#hamburger-menu');
+const mobileMenu = document.querySelector('#mobile-menu');
+const closeMenu = document.querySelector('#close-menu');
+const overlay = document.querySelector('#overlay');
+
+hamburgerMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    overlay.classList.toggle('hidden');
+    overlay.classList.toggle('active');
+});
+
+closeMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    overlay.classList.toggle('hidden');
+    overlay.classList.toggle('active');
+});
