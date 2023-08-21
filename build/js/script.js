5b113769b617e1713b9a1cd17f48dc6c60df42ad
Khalid <mahamud.0.khalid@gmail.com>
2023-08-22 02:25:49 +0600

    completed slider

--- build/js/script.js
+++ build/js/script.js
@@ -2,6 +2,10 @@ const hamburgerMenu = document.querySelector('#hamburger-menu');
 const mobileMenu = document.querySelector('#mobile-menu');
 const closeMenu = document.querySelector('#close-menu');
 const overlay = document.querySelector('#overlay');
+const productImg = document.querySelectorAll('.carousel-img');
+const productThumb = document.querySelectorAll('#thumbnail div');
+const prevBtn = document.querySelector('#prev-btn');
+const nextBtn = document.querySelector('#next-btn');
 
 hamburgerMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
@@ -13,4 +17,24 @@ closeMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
     overlay.classList.toggle('hidden');
     overlay.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target.id === 'overlay') {
+        mobileMenu.classList.toggle('active');
+        overlay.classList.toggle('hidden');
+        overlay.classList.toggle('active');
+    }
+});
+
+let current = 0;
+
+nextBtn.addEventListener('click', () => {
+    productImg[current].setAttribute('aria-current', 'false');
+    productThumb[current].setAttribute('aria-current', 'false');
+
+    current = (current + 1) % 4;
+
+    productImg[current].setAttribute('aria-current', 'true');
+    productThumb[current].setAttribute('aria-current', 'true');
 });
